e2cfde781f9463c4afa1ac9b5fc8e520ef408261
Szabó Gergő <gergo85@gmail.com>
2017-06-16 10:43:19 +0200

    Translate the plugin details

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,5 +1,9 @@
 <?php return [
     'inetis' => [
+        'plugin' => [
+            'name'        => 'List Switch',
+            'description' => 'Add support of clickable switch in backend list.',
+        ],
         'listswitch' => [
             'title_true'  => 'Click to switch to No',
             'title_false' => 'Click to switch to Yes',
