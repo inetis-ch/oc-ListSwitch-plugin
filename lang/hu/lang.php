3ceb61be0584856610912cf1264d4d94037f5e3d
Szabó Gergő <gergo85@gmail.com>
2017-06-16 10:43:50 +0200

    Add the Hungarian translation

---
+++ lang/hu/lang.php
@@ -0,0 +1,14 @@
+<?php return [
+    'inetis' => [
+        'plugin' => [
+            'name'        => 'Gyorskapcsoló listákhoz',
+            'description' => 'Kattintható mező lehetőség hozzádása az admin listákhoz.',
+        ],
+        'listswitch' => [
+            'title_true'  => 'Váltás erre: Nem',
+            'title_false' => 'Váltás erre: Igen',
+            'text_true'   => 'Igen',
+            'text_false'  => 'Nem',
+        ],
+    ],
+];
